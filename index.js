695a9121204ec9695d0a2dfe87ee5e299401c926
Bharat <bharat17oct@gmail.com>
2021-06-29 15:48:31 -0400

    Updated index.js to handle Thousand of http call

--- index.js
+++ index.js
@@ -1,29 +1,87 @@
+console.log("Start");
+let AdFoundArr = [];
 
-
-//SetTimeout Example
-
-//Output - Start --> End --> CallBack
-let time = 3000;
-console.log("Start before setTimeout");
-setTimeout(function(){
-    console.log("CallBack after "+time+ "ms");
-},time);
-console.log("End after setTimeout");
-
-
-//setTimeOutInLoop
-//Output - etTimeOutInLoop - Start before setTimeout 
-//--> setTimeOutInLoop - End after setTimeout 
-//--> 6 TimessetTimeOutInLoop - CallBack after 6000ms because i value is 6 by the the settimeout comes to call stack
-
-console.log("setTimeOutInLoop - Start before setTimeout");
-function setTimeOutInLoop(n){
-     for(var i=0;i<=n;i++){
-        setTimeout(function(){
-            console.log("setTimeOutInLoop - CallBack after "+i*1000+ "ms");
-        },i*1000);
-     }
+function asynCalltoFile(file){
+        fetch(file).then((response)=>{
+            return response.text();
+        }).then((text)=>{
+            getData(text);
+        })
 }
-console.log("setTimeOutInLoop - End after setTimeout");
 
-setTimeOutInLoop(5);
+//asynCalltoFile("wurl_1.5");
+//asynCalltoFile("test");
+asynCalltoFile("wurl_4Days");
+
+function getData(data){
+   let splitNewLine = data.split('\n'),eachRecord;
+   let noOfLines = splitNewLine.length;
+    //console.log(noOfLines)
+    let i =0,x=[];
+    for(let i=0;i<noOfLines;i++){
+        eachRecord = splitNewLine[i].match(/(?<=GET\s+).*?(?=\s+HTTP)/gs);
+        auditude(eachRecord,i);
+    }
+    
+}
+
+setTimeout(function(){
+    console.log(AdFoundArr);
+},120000)
+
+function auditude(record,requestIndex){
+    if(record!=null){
+        let audDomain = "http://ad.auditude.com"
+        let auditudeURL = audDomain + record;
+        getXMLResponse(auditudeURL,requestIndex);
+    }
+}
+
+function getXMLResponse(url,requestIndex){
+        let x = new XMLHttpRequest();
+        x.open("GET", url, true);
+        x.onreadystatechange = function () {
+        if (x.readyState == 4 && x.status == 200){
+            let doc = x.responseXML;
+            x = doc.documentElement.childNodes;
+            var txt,adDetails,j=0;
+            for (i = 0; i < x.length ;i++) {
+                txt = x[i].nodeName; 
+                if (txt == "Body"){   
+                     pod = x[i].getElementsByTagName("Pod")[0];
+                        if(pod){
+                            if(pod.getElementsByTagName("Lot")[0]){
+                                adDetails = pod.getElementsByTagName("Lot")[0].getElementsByTagName("Ad")[0];
+                                if(adDetails){
+                                    adSystem = adDetails.getElementsByTagName("AdSystem")[0].textContent;
+                                    redirectURL = adDetails.getElementsByTagName("AdTagURI")[0].textContent;
+                                    adID = adDetails.getAttribute("id");
+                                    AdFoundArr.push(url+" "+adID+" "+adSystem+" "+redirectURL);
+                                    //console.log(adID,adSystem,redirectURL);
+                                    console.log(requestIndex,"Ad Found");
+                                }
+                                else {
+                                  //  console.log(requestIndex,"No Ad Found");
+                                //console.log(i);
+                                }
+                            }
+                            else {
+                               // console.log(requestIndex,"No Lot Found");
+                            }
+                            
+                        }    
+                        else {
+                            //console.log(requestIndex,"No Pod Found");
+
+                        }             
+                 }        
+            }
+            //
+        }
+    };
+    x.send(null);
+    //return AdFoundArr;
+} 
+
+
+//console.log(AdFoundArr.length);
