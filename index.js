43a40389d41fc1114ffae62fff9e5e98b5256d42
Bharat <bharat17oct@gmail.com>
2021-07-01 22:54:38 +0530

    Changes done ro Compare array, Remove query Para,
    1. Compared Adios vs Auditude Array.
    2.  Removed URL from Array Content
    3. Remove "cb" param from URL, it is causing issue when comparing two arrays

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log("Start");
-let AdFoundArr = [];
+let AdFoundArrAdios = [];
+let AdFoundArrAuditude = [];
 
 function asynCalltoFile(file){
         fetch(file).then((response)=>{
@@ -18,26 +19,40 @@ function getData(data){
    let noOfLines = splitNewLine.length;
     //console.log(noOfLines)
     let i =0,x=[];
-    for(let i=0;i<noOfLines;i++){
+    for(let i=0;i<500;i++){
         eachRecord = splitNewLine[i].match(/(?<=GET\s+).*?(?=\s+HTTP)/gs);
+        adios(eachRecord,i);
         auditude(eachRecord,i);
     }
     
 }
 
 setTimeout(function(){
-    console.log(AdFoundArr);
-},120000)
+    AdFoundArrAdios = AdFoundArrAdios.sort();
+    AdFoundArrAuditude = AdFoundArrAuditude.sort();
+    let difference = AdFoundArrAuditude
+                 .filter(x => !AdFoundArrAdios.includes(x))
+                 .concat(AdFoundArrAdios.filter(x => !AdFoundArrAuditude.includes(x)));
+    console.log(difference);
+},60000)
+
+function adios(record,requestIndex){
+    if(record!=null){
+        let audDomain = "http://ad.primetime.adobe.com"
+        let auditudeURL = audDomain + record;
+        getXMLResponse(auditudeURL,requestIndex,audDomain,record);
+    }
+}
 
 function auditude(record,requestIndex){
     if(record!=null){
         let audDomain = "http://ad.auditude.com"
         let auditudeURL = audDomain + record;
-        getXMLResponse(auditudeURL,requestIndex);
+        getXMLResponse(auditudeURL,requestIndex,audDomain,record);
     }
 }
 
-function getXMLResponse(url,requestIndex){
+ function getXMLResponse(url,requestIndex,domain,record){
         let x = new XMLHttpRequest();
         x.open("GET", url, true);
         x.onreadystatechange = function () {
@@ -55,10 +70,20 @@ function getXMLResponse(url,requestIndex){
                                 if(adDetails){
                                     adSystem = adDetails.getElementsByTagName("AdSystem")[0].textContent;
                                     redirectURL = adDetails.getElementsByTagName("AdTagURI")[0].textContent;
+                                    redirectURL=removeURLParameter_cb(redirectURL);
                                     adID = adDetails.getAttribute("id");
-                                    AdFoundArr.push(url+" "+adID+" "+adSystem+" "+redirectURL);
+                                    if(domain=="http://ad.auditude.com"){
+                                        AdFoundArrAuditude.push(record+" "+adSystem+" "+redirectURL);
+                                       // AdFoundArrAuditude.push(record);
+                                      //  AdFoundArrAuditude.push(adSystem);
+                                      //  AdFoundArrAuditude.push(redirectURL);
+                                    }
+                                    if(domain=="http://ad.primetime.adobe.com"){
+                                        AdFoundArrAdios.push(record+" "+adSystem+" "+redirectURL);    
+                                    }
+                                    
                                     //console.log(adID,adSystem,redirectURL);
-                                    console.log(requestIndex,"Ad Found");
+                                   // console.log(requestIndex,"Ad Found");
                                 }
                                 else {
                                   //  console.log(requestIndex,"No Ad Found");
@@ -82,6 +107,30 @@ function getXMLResponse(url,requestIndex){
     x.send(null);
     //return AdFoundArr;
 } 
+
+
+function removeURLParameter_cb(url) {
+    //prefer to use l.search if you have a location/link object
+    var urlparts= url.split('?');   
+    if (urlparts.length>=2) {
+
+        var prefix= encodeURIComponent("cb")+'=';
+        var pars= urlparts[1].split(/[&;]/g);
+
+        //reverse iteration as may be destructive
+        for (var i= pars.length; i-- > 0;) {    
+            //idiom for string.startsWith
+            if (pars[i].lastIndexOf(prefix, 0) !== -1) {  
+                pars.splice(i, 1);
+            }
+        }
+
+        url= urlparts[0]+'?'+pars.join('&');
+        return url;
+    } else {
+        return url;
+    }
+}
 
 
 //console.log(AdFoundArr.length);
