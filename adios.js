265c9af89b60fae265fdfb690169a97a00a7026d
Bharat <bharat17oct@gmail.com>
2021-07-03 21:35:11 +0530

    Exporting URL of "No Ads"in CSV

--- adios.js
+++ adios.js
@@ -1,5 +1,8 @@
 console.log("Start");
 let AdFoundArrAdios = [];
+let AdFoundArrAuditude = [];
+let NoAdAuditude = [];
+var request=0;
 
 function asynCalltoFile(file){
         fetch(file).then((response)=>{
@@ -9,35 +12,86 @@ function asynCalltoFile(file){
         })
 }
 
-//asynCalltoFile("wurl_1.5");
-//asynCalltoFile("test");
+
 asynCalltoFile("wurl_4Days");
 
-function getData(data){
+async function getData(data){
    let splitNewLine = data.split('\n'),eachRecord;
    let noOfLines = splitNewLine.length;
-    //console.log(noOfLines)
-    let i =0,x=[];
-    for(let i=0;i<10000;i++){
-        eachRecord = splitNewLine[i].match(/(?<=GET\s+).*?(?=\s+HTTP)/gs);
-        auditude(eachRecord,i);
+    console.log(noOfLines);
+    forLoop = async(i) => {
+        for(i=0;i<100;i++){
+            eachRecord = splitNewLine[i].match(/(?<=GET\s+).*?(?=\s+HTTP)/gs);
+            adios(eachRecord,i);
+            auditude(eachRecord,i);
+            request =i;
+            await itemRunner(i);
+        }
     }
-    
+    forLoop();
 }
 
-setTimeout(function(){
-    console.log(AdFoundArrAdios);
-},700000)
+async function itemRunner(item){
+    await delay();
+    //console.log(item);
+}
+var countPromise=0;
+function delay(){
+    return new Promise((resolve, reject) => {
+        setTimeout(resolve, 200);
+    })
+        
+ }
 
-function auditude(record,requestIndex){
+setTimeout(function(){
+    AdFoundArrAdios = AdFoundArrAdios.sort();
+    AdFoundArrAuditude = AdFoundArrAuditude.sort();
+    diffValue = AdFoundArrAuditude.length - AdFoundArrAdios.length;
+    console.log("Difference is - "+diffValue+" out of Total "+request+1+" request");
+
+    let difference = AdFoundArrAuditude
+                 .filter(x => !AdFoundArrAdios.includes(x))
+                 .concat(AdFoundArrAdios.filter(x => !AdFoundArrAuditude.includes(x)));
+    console.log(difference);
+    //generateCSV(difference);
+    generateCSV(NoAdAuditude);
+
+
+},10000)
+
+function generateCSV(data){
+    var CsvString = "";
+    data.forEach(function(RowItem, RowIndex) {
+        CsvString += RowItem + ',';
+        CsvString += "\r\n";
+    });
+    CsvString = "data:application/csv," + encodeURIComponent(CsvString);
+    var x = document.createElement("A");
+    x.setAttribute("href", CsvString );
+    x.setAttribute("download","ABDiffResult.csv");
+    document.body.appendChild(x);
+    x.click()
+}
+
+
+
+function adios(record,requestIndex){
     if(record!=null){
         let audDomain = "http://ad.primetime.adobe.com"
         let auditudeURL = audDomain + record;
-        getXMLResponse(auditudeURL,requestIndex);
+        getXMLResponse(auditudeURL,requestIndex,audDomain,record);
     }
 }
 
- function getXMLResponse(url,requestIndex){
+function auditude(record,requestIndex){
+    if(record!=null){
+        let audDomain = "http://ad.auditude.com"
+        let auditudeURL = audDomain + record;
+        getXMLResponse(auditudeURL,requestIndex,audDomain,record);
+    }
+}
+
+ function getXMLResponse(url,requestIndex,domain,record){
         let x = new XMLHttpRequest();
         x.open("GET", url, true);
         x.onreadystatechange = function () {
@@ -55,23 +109,34 @@ function auditude(record,requestIndex){
                                 if(adDetails){
                                     adSystem = adDetails.getElementsByTagName("AdSystem")[0].textContent;
                                     redirectURL = adDetails.getElementsByTagName("AdTagURI")[0].textContent;
+                                    redirectURL=removeURLParameter_cb(redirectURL);
                                     adID = adDetails.getAttribute("id");
-                                    AdFoundArrAdios.push(url+" "+adID+" "+adSystem+" "+redirectURL);
+                                    if(domain=="http://ad.auditude.com"){
+                                        //AdFoundArrAuditude.push(record+" "+adSystem+" "+redirectURL);
+                                       // AdFoundArrAuditude.push(record);
+                                      //  AdFoundArrAuditude.push(adSystem);
+                                      //  AdFoundArrAuditude.push(redirectURL);
+                                    }
+                                    if(domain=="http://ad.primetime.adobe.com"){
+                                       // AdFoundArrAdios.push(record+" "+adSystem+" "+redirectURL);    
+                                    }
+                                    
                                     //console.log(adID,adSystem,redirectURL);
-                                   // console.log(requestIndex,"Ad Found");
+                                    //console.log(requestIndex,"Ad Found");
                                 }
                                 else {
-                                  //  console.log(requestIndex,"No Ad Found");
+                                    console.log(requestIndex,"No Ad Found");
                                 //console.log(i);
                                 }
                             }
                             else {
-                               // console.log(requestIndex,"No Lot Found");
+                                console.log(requestIndex,"No Lot Found");
+                                NoAdAuditude.push(url);
                             }
                             
                         }    
                         else {
-                            //console.log(requestIndex,"No Pod Found");
+                            console.log(requestIndex,"No Pod Found "+ pod);
 
                         }             
                  }        
@@ -82,6 +147,29 @@ function auditude(record,requestIndex){
     x.send(null);
     //return AdFoundArr;
 } 
+
+
+function removeURLParameter_cb(url) {
+    var urlparts= url.split('?');   
+    if (urlparts.length>=2) {
+
+        var prefix= encodeURIComponent("cb")+'=';
+        var pars= urlparts[1].split(/[&;]/g);
+
+        //reverse iteration as may be destructive
+        for (let i= pars.length; i-- > 0;) {    
+            //idiom for string.startsWith
+            if (pars[i].lastIndexOf(prefix, 0) !== -1) {  
+                pars.splice(i, 1);
+            }
+        }
+
+        url= urlparts[0]+'?'+pars.join('&');
+        return url;
+    } else {
+        return url;
+    }
+}
 
 
 //console.log(AdFoundArr.length);
